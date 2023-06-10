120e924b52d35fa67492c09a927b4c0b94cb6312
bindum1199 <89576533+bindum1199@users.noreply.github.com>
2023-06-10 12:26:35 +0530

    Update dockerrun.sh

--- scripts/dockerrun.sh
+++ scripts/dockerrun.sh
@@ -1,3 +1,3 @@
-sudo docker pull yedukondalu212/java-app-demo:latest
+sudo docker pull bindum1199/java-app-demo:latest
 sudo docker rm -f javaapp
-sudo docker run -itd -p 8004:8080 --name javaapp yedukondalu212/java-app-demo:latest
+sudo docker run -itd -p 8004:8080 --name javaapp bindum1199/java-app-demo:latest
