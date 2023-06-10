e93cfe0c37516b8cc7e93af34d82f6a15588621c
bindum1199 <89576533+bindum1199@users.noreply.github.com>
2023-06-10 20:03:19 +0530

    Update dockerpush.sh

--- scripts/dockerpush.sh
+++ scripts/dockerpush.sh
@@ -1,5 +1,5 @@
 
 #sudo docker login --username=$username --password=$password
 sudo docker logout
-cat ~/password.txt | sudo docker login --username bindum1199 --password-stdin
-sudo docker push bindum1199/java-app-demo:latest
+cat ~/password.txt | sudo docker login --username bindum11 --password-stdin
+sudo docker push bindum11/java-app-demo:latest
